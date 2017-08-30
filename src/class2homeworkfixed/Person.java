2cadeacb91b000ae15ccd4b51028ed686b6ae8b4
Cainebourne <Cainebourne@192.168.2.83>
2017-08-30 00:09:10 -0500

    created variables

--- src/class2homeworkfixed/Person.java
+++ src/class2homeworkfixed/Person.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package class2homeworkfixed;
 
 /**
@@ -10,5 +6,7 @@ package class2homeworkfixed;
  * @author Cainebourne
  */
 public class Person {
-    
+    private String firstName;
+    private String lastName;
+    private int age;
 }
