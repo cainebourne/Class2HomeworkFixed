d1c41a26ada827e65236d4fbbf0527b9773d1729
Cainebourne <Cainebourne@192.168.2.83>
2017-08-30 00:13:42 -0500

    finished!

--- src/class2homeworkfixed/Class2HomeworkFixed.java
+++ src/class2homeworkfixed/Class2HomeworkFixed.java
@@ -8,7 +8,16 @@ package class2homeworkfixed;
 public class Class2HomeworkFixed {
 
     public static void main(String[] args) {
-        
+         Person p = new Person("John", "Miller", 35);
+         Person p2 = new Person("Mary", "Miller", 32);
+
+   System.out.println("First Name: " + p.getFirstName());
+   System.out.println("Last Name: " + p.getLastName());
+   System.out.println("Age: " + p.getAge());
+   
+   System.out.println("First Name: " + p2.getFirstName());
+   System.out.println("Last Name: " + p2.getLastName());
+   System.out.println("Age: " + p2.getAge());
     }
     
 }
